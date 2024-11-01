ca41413cb12ada4036bdef47dca39da07d6d6041
Kylix Eza S <ksaputra@dopalearn.com>
2024-11-01 14:25:44 +0700

    style(search): renamed label to make it clarify

--- home/src/commonMain/kotlin/com.kylix/home/screens/search/SearchScreen.kt
+++ home/src/commonMain/kotlin/com.kylix/home/screens/search/SearchScreen.kt
@@ -1,14 +1,21 @@
 package com.kylix.home.screens.search
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -53,11 +60,9 @@ class SearchScreen: Screen {
                     BeuBasicTextField(
                         value = searchState.displayQuery,
                         onValueChange = { screenModel.setQuery(it) },
-                        label = "Search",
+                        label = "Search recipes by name or ingredients",
                         modifier = Modifier.fillMaxWidth(),
                         singleLine = true,
-                        trailingIcon = {
-                        }
                     )
                 }
             },
