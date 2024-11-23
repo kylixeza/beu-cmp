2f5065cffa97eb4d41d83026c5a3495401b5f3ee
Kylix Eza S <ksaputra@dopalearn.com>
2024-11-23 13:09:25 +0700

    refactor(search): add circle loading

--- home/src/commonMain/kotlin/com.kylix/home/screens/search/SearchScreen.kt
+++ home/src/commonMain/kotlin/com.kylix/home/screens/search/SearchScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +16,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -70,6 +71,20 @@ class SearchScreen: Screen {
             },
             statusBarColor = Primary500,
         ) { innerPadding ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                AnimatedVisibility(
+                    modifier = Modifier.align(Alignment.Center),
+                    visible = uiState.isLoading && searchState.recipes.isEmpty(),
+                    enter = fadeIn() + scaleIn(),
+                    exit = scaleOut(),
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(48.dp),
+                        color = Primary500,
+                    )
+                }
+            }
+
             LazyColumn(
                 modifier = Modifier.fillMaxWidth().padding(innerPadding),
                 contentPadding = PaddingValues(horizontal = 24.dp, vertical = 8.dp)
